0b843303a2a2e6feae4f14aa9da335c1f1dfd925
DaviSMoura <davisilvag4mer@gmail.com>
2020-12-23 23:48:41 +0100

    feat: roll 1

--- commands/rpg/roll.js
+++ commands/rpg/roll.js
@@ -2,10 +2,19 @@ const Discord = require("discord.js")
 
 exports.run = async (bot, message, args) => {
     if(!args[0]) return message.reply("**Digite o número de lados.**\n`.roll d20`");
-
-    message.delete().catch(O_o=>{});
     
+    var nmr = args[0];
+    nmr = nmr.substring(nmr.indexOf("d"));
+    nmr = nmr.split("d")[1];
+    var qtd = args[0].split("d")[0];
+    
+    if (qtd != "" || qtd == null) {
+        
+    } else return message.reply("Número: " + getRandom(1,nmr));
+}
 
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 exports.help = {
