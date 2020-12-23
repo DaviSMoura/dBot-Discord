e8ca28a9347f11454fe8c4ec45500094f1c90b32
DaviSMoura <davisilvag4mer@gmail.com>
2020-12-23 23:19:36 +0100

    + Event Manager

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const fs = require("fs");
 const fileUtils = require("./utils/fileUtils");
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
 function iniciar() {
-    
+    console.log("Carregando eventos...");
+    carregarEventos();
+    console.log("Carregando comandos...");
+    carregarComandos();
     
     console.log("Conectando...");
     client.login(config);
 }
 
 function carregarComandos() {
-    for (const dirInfo of fileUtils.searchByExtension("commands", 'js')) {
+    for (const dirInfo of fileUtils.searchByExtension("./commands", 'js')) {
         const dirList = dirInfo.directory.split('/');
         dirList.shift();
         dirList.shift();
@@ -36,6 +41,27 @@ function carregarComandos() {
 
         const formatedFiles = dirInfo.files.map(file => file.split('/').pop().split('.').shift())
         console.log(`[Gerenciador de Comandos] Foram carregados ` + dirInfo.files.length + ' comandos na categoria ' + commandCategory + '. [' + formatedFiles.join(', ') + ']');
+    }
+}
+
+function carregarEventos(dir) {
+    for (const dirInfo of fileUtils.searchByExtension("./events", 'js')) {
+        for (const file of dirInfo.files) {
+            let events = require(file);
+            if(!Array.isArray(events)) {
+                events = [events];
+            }
+
+            for (const event of events) {
+                if(!event.name || !event.run) {
+                    continue;
+                }
+    
+                console.log(`[Gerenciador de Eventos] O evento ` + event.name + ' foi carregado!');
+    
+                client.on(event.name, (...args) => event.run(client, ...args));
+            }
+        }
     }
 }
 
